89bb2d638fd3b7a950f5960e5503b07af981f41d
Lekau Boitumelo <boitumelolekau09@gmail.com>
2025-07-07 22:10:48 +0200

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -12,7 +12,7 @@ $error_message = '';
 $send_error = '';
 $cash_out_error = '';
 
-$conn = new mysqli("localhost", "root", "", "Mobipay");
+$conn = new mysqli("localhost", "root", "", "mobipay_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
