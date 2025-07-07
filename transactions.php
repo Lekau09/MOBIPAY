1604633e934c88a4ff660182c27afff3408381cc
Lekau Boitumelo <boitumelolekau09@gmail.com>
2025-07-07 22:16:11 +0200

    transactions.php

--- transactions.php
+++ transactions.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     exit();
 }
 
-$conn = new mysqli("localhost", "root", "", "Mobipay");
+$conn = new mysqli("localhost", "root", "", "mobipay_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
